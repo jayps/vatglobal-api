000e03779b838f373db4dad09736d9258f9fcddf
JP Meyer <jp@jpmeyer.dev>
2022-03-17 21:10:18 +0200

    Added file upload acceptance

--- vatglobal/api/views.py
+++ vatglobal/api/views.py
@@ -1,8 +1,23 @@
+import csv
+import io
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from vatglobal.api.serializers import UploadSerializer
+
 
 class TransactionUploadView(APIView):
     def post(self, request, format=None):
-        return Response(data={'test': 'data'}, status=status.HTTP_201_CREATED)
+        serializer = UploadSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        file = request.FILES['file']
+        decoded_file = file.read().decode('UTF-8')
+        csv_string = io.StringIO(decoded_file)
+        for line in csv.reader(csv_string, delimiter=','):
+            print(line)
+
+
+        return Response(data=serializer.validated_data, status=status.HTTP_201_CREATED)
