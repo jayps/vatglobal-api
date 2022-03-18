695b68d5e2bfe108c495ede4b14d7e6ccb1059f2
JP Meyer <jp@jpmeyer.dev>
2022-03-18 16:29:24 +0200

    Adjusted country filter for ISO-3166-1 alpha-2

--- vatglobal/api/filters.py
+++ vatglobal/api/filters.py
@@ -7,9 +7,10 @@ from vatglobal.api.models import Transaction
 class TransactionFilter(django_filters.FilterSet):
     date = filters.DateFilter()
 
-    # I'm allowing any character filter for country since ISO-3166 allows for full country names, and ISO-3166-1
-    # allows for alpha-2 and alpha-3 codes as at https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes.
-    country = filters.CharFilter()
+    # Allowing for 2 character ISO-3166-1 (alpha-2) codes as per the example in the instructions, such as
+    # /retrieveRows?country=DE&date=2020/08/05&currency=GBP
+    # see: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes.
+    country = filters.CharFilter(max_length=2, min_length=2)
 
     class Meta:
         model = Transaction
